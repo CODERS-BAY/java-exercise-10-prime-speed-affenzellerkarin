896710f7eb95ba252742e4565e2f27db35467206
affenzellerkarin <karinaffenzeller@yahoo.de>
2022-04-22 23:28:38 +0200

    finished

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,17 +1,70 @@
 package application;
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
+
+
+		System.out.println("Ausgabe der Primzahlen  -----------------------------------");
+		int zahlenumfang;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Wie viele Primzahlen sollen ermittelt werden?");
+		zahlenumfang = scanner.nextInt();
+
+		PrimeNumbers pr = new PrimeNumbers();
+		int [] primes = new int[zahlenumfang];
+
 		long start = System.currentTimeMillis();
-		int[] primes = null; // replace with correct call
+
+		primes=pr.getPrimeNumbers(zahlenumfang);
+
+//		int zahl=3;
+//		int[] primes = new int[zahlenumfang];
+//		primes[0]=2;
+//		boolean pz = true;
+//
+//
+//		int counter=0;
+//
+//		while (counter <zahlenumfang-1) {
+//			pz=true;
+//			int i=0;
+//			double wurzel = Math.sqrt(zahl);
+//			if (zahl % wurzel == 0) {
+//				pz = false;}
+//
+//			else {
+//				while (pz && i < counter && primes[i] <= wurzel) {
+////			while (pz && i<counter && primes[i]<=Math.sqrt(zahl)) {   // T20 = { 1, 2, 4, 5, 10, 20}, T9 = { 1, 3, 9}
+//
+//					if (zahl % primes[i] == 0) {
+//						pz = false;
+//					}
+//					i++;
+//				}
+//			}
+//
+//			if (pz) {
+//				counter++;
+//				primes[counter]=zahl;
+//			}
+//			zahl=zahl+2; // ab 3 nur die ungeraden Zahlen
+//		}
+//
+//
+//
+
 		long end = System.currentTimeMillis();
-		
-		for (int prime : primes) {
-			System.out.print(prime + " ");
-		}
 
 		System.out.println();
+
+		for (int prime: primes) {
+			System.out.println(prime);
+		}
 		System.out.println("It took " + ((end - start) / 1000) + " seconds");
+
 	}
 }
--- src/main/java/application/PrimeNumbers.java
+++ src/main/java/application/PrimeNumbers.java
@@ -11,6 +11,40 @@ public class PrimeNumbers {
 	 * @return an array containing only the calculated prime numbers
 	 */
 	public int[] getPrimeNumbers(int amountOfPrimeNumbers) {
-		return null;
+
+		int zahl=3;
+		int[] primes = new int[amountOfPrimeNumbers];
+		primes[0]=2;
+		boolean pz = true;
+
+		int counter=0;
+		while (counter < amountOfPrimeNumbers -1) {
+			pz=true;
+			int i=0;
+			double wurzel = Math.sqrt(zahl);
+			// Überprüft, ob die Zahl eine Quadratzahl ist --> falls ja, dann ist sie keine Primzahl
+			if (zahl % wurzel == 0) {
+				pz = false;
+			}
+			// Jede Zahl lässt sich in Primfaktoren aufteilen, die kleiner gleich der Zahl sind.
+			// Es genügt also, die bereits ermittelten Primzahlen als Teiler zu prüfen
+			else {
+				while (pz && i < counter && primes[i] <= wurzel) {
+					if (zahl % primes[i] == 0) {
+						pz = false;
+						break;
+					}
+					i++;
+				}
+			}
+			//Falls es keine Teiler kleiner der Zahl gibt, ist es eine Primzahl
+			if (pz) {
+				counter++;
+				primes[counter]=zahl;
+			}
+			zahl=zahl+2; // ab 3 werden nur die ungeraden Zahlen überprüft
+		}
+
+		return primes;
 	}
 }
